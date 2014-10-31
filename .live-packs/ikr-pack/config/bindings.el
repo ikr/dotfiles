e0b99140ce21fa0bae95159a83a83b7cd2b8997c
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-10-31 11:22:11 +0100

    The new code for camelCase <-> snake_case toggling

--- .live-packs/ikr-pack/config/bindings.el
+++ .live-packs/ikr-pack/config/bindings.el
@@ -22,30 +22,33 @@
                 (max (point) (mark))
                 (- 4))))))
 
-;---------------------------------------------------------------------------------------------------
+;;--------------------------------------------------------------------------------------------------
 
-(defun camelize (s)
-  "Convert under_score string S to CamelCase string."
-  (mapconcat
-   'identity
-   (mapcar
-    '(lambda (word) (capitalize (downcase word)))
-    (split-string s "_"))
-   ""))
+(defun mo-toggle-identifier-naming-style ()
+  "Toggles the symbol at point between C-style naming,
+e.g. `hello_world_string', and camel case,
+e.g. `HelloWorldString'."
+  (interactive)
+  (let* ((symbol-pos (bounds-of-thing-at-point 'symbol))
+         case-fold-search symbol-at-point cstyle regexp func)
+    (unless symbol-pos
+      (error "No symbol at point"))
+    (save-excursion
+      (narrow-to-region (car symbol-pos) (cdr symbol-pos))
+      (setq cstyle (string-match-p "_" (buffer-string))
+            regexp (if cstyle "\\(?:\\_<\\|_\\)\\(\\w\\)" "\\([A-Z]\\)")
+            func (if cstyle
+                     'capitalize
+                   (lambda (s)
+                     (concat (if (= (match-beginning 1)
+                                    (car symbol-pos))
+                                 ""
+                               "_")
+                             (downcase s)))))
+      (goto-char (point-min))
+      (while (re-search-forward regexp nil t)
+        (replace-match (funcall func (match-string 1))
+                       t nil))
+      (widen))))
 
-(defun camelize-previous-snake (&optional beg end)
-  "Camelize the previous snake cased string.
-
-    If transient-mark-mode is active and a region is activated,
-    camelize the region."
-  (interactive "r")
-  (unless (and (boundp 'transient-mark-mode) transient-mark-mode mark-active)
-    (setq end (point)
-          beg (+ (point) (skip-chars-backward "[:alnum:]_"))))
-  (save-excursion
-    (let ((c (camelize (buffer-substring-no-properties beg end))))
-      (delete-region beg end)
-      (goto-char beg)
-      (insert c))))
-
-(global-set-key (kbd "M-_") 'camelize-previous-snake)
+(global-set-key (kbd "M--") 'mo-toggle-identifier-naming-style)

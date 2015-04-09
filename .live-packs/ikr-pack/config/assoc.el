e2e51be976a072c7da7bb11a97264c79f3a3ead2
Ivan Krechetov <ikr@ikr.su>
2015-04-09 14:49:59 +0200

    js-mode for .es

--- .live-packs/ikr-pack/config/assoc.el
+++ .live-packs/ikr-pack/config/assoc.el
@@ -2,4 +2,5 @@
 (setq auto-mode-alist (cons '(".*\.tmpl" . html-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '(".*\.twig" . html-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '(".*\.es6" . js-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '(".*\.es" . js-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '(".*\.jsx" . js-mode) auto-mode-alist))

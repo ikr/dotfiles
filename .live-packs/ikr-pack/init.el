c8bd7a346443b7d5ee945e91968461e55ac3fdb8
Ivan Krechetov <ikr@ikr.su>
2015-07-07 11:38:39 +0200

    Dump the pi-php-mode

--- .live-packs/ikr-pack/init.el
+++ .live-packs/ikr-pack/init.el
@@ -6,9 +6,6 @@
 (live-load-config-file "bindings.el")
 (live-load-config-file "assoc.el")
 
-(live-add-pack-lib "pi-php-mode")
-(require 'pi-php-mode)
-
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
